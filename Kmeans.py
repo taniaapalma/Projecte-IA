29bbc828a9b34640b08cb4ee626b68a644afb619
KNKN <1673296@uab.cat>
2025-03-26 13:31:19 +0100

    Funcion init_x

--- Kmeans.py
+++ Kmeans.py
@@ -1,5 +1,5 @@
-__authors__ = 'TO_BE_FILLED'
-__group__ = 'TO_BE_FILLED'
+__authors__ = ['1673296','1674485','1669906']
+__group__ = '11'
 
 import numpy as np
 import utils
@@ -34,7 +34,12 @@ class KMeans:
         ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
         ##  AND CHANGE FOR YOUR OWN CODE
         #######################################################
-        self.X = np.random.rand(100, 5)
+        
+        npArray = np.array(X,dtype=float)
+        if(npArray.ndim == 3):
+            npArray = npArray.reshape(-1,3)
+        self.X = npArray
+
 
     def _init_options(self, options=None):
         """
@@ -71,12 +76,12 @@ class KMeans:
         ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
         ##  AND CHANGE FOR YOUR OWN CODE
         #######################################################
-        if self.options['km_init'].lower() == 'first':
-            self.centroids = np.random.rand(self.K, self.X.shape[1])
-            self.old_centroids = np.random.rand(self.K, self.X.shape[1])
-        else:
-            self.centroids = np.random.rand(self.K, self.X.shape[1])
-            self.old_centroids = np.random.rand(self.K, self.X.shape[1])
+        # if self.options['km_init'].lower() == 'first':
+        #     self.centroids = np.random.rand(self.K, self.X.shape[1])
+        #     self.old_centroids = np.random.rand(self.K, self.X.shape[1])
+        # else:
+        #     self.centroids = np.random.rand(self.K, self.X.shape[1])
+        #     self.old_centroids = np.random.rand(self.K, self.X.shape[1])
 
     def get_labels(self):
         """

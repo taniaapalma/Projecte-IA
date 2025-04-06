5bd05a6cc46130e27a8f1ecbf3666c39a898d115
KNKN <1673296@uab.cat>
2025-04-06 11:55:44 +0200

    KNN, init train hecho

--- KNN.py
+++ KNN.py
@@ -1,5 +1,5 @@
-__authors__ = 'TO_BE_FILLED'
-__group__ = 'TO_BE_FILLED'
+__authors__ = ['1673296','1674485','1669906']
+__group__ = '11'
 
 import numpy as np
 import math
@@ -21,11 +21,8 @@ class KNN:
         :param train_data: PxMxNx3 matrix corresponding to P color images
         :return: assigns the train set to the matrix self.train_data shaped as PxD (P points in a D dimensional space)
         """
-        #######################################################
-        ##  YOU MUST REMOVE THE REST OF THE CODE OF THIS FUNCTION
-        ##  AND CHANGE FOR YOUR OWN CODE
-        #######################################################
-        self.train_data = np.random.randint(8, size=[10, 4800])
+        data = np.array(train_data, dtype=float)
+        self.train_data = data.reshape(train_data.shape[0], -1)
 
     def get_k_neighbours(self, test_data, k):
         """
